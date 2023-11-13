86cd8028dac50b28793abb8b18e49ea685491706
mariawasilewa-coca-cola <marvasileva@coca-cola.com>
2023-11-13 19:46:02 +0200

    updated app.py

--- app/app.py
+++ app/app.py
@@ -1,9 +1,7 @@
 import os
-
 from flask import Flask
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello_world():
@@ -12,4 +10,3 @@ def hello_world():
 
 if __name__ == "__main__":
     app.run(port=os.environ.get("PORT", 5000), host="0.0.0.0")
-    
