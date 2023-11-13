1a5c28c0db2a88f4323bd9b4d939d336951c2c9c
mariawasilewa-coca-cola <marvasileva@coca-cola.com>
2023-11-13 19:47:29 +0200

    updated app.py

--- app/app.py
+++ app/app.py
@@ -1,3 +1,7 @@
+"""
+This module contains the main Flask application.
+"""
+
 import os
 from flask import Flask
 
@@ -5,8 +9,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    """
+    Route for the root endpoint.
 
+    Returns:
+        str: A greeting message.
+    """
+    return "Hello, World!"
 
 if __name__ == "__main__":
     app.run(port=os.environ.get("PORT", 5000), host="0.0.0.0")
