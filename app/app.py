867f6b7e804423444798987705f913c9765eb26a
mariawasilewa-coca-cola <marvasileva@coca-cola.com>
2023-11-13 20:25:29 +0200

    remove comments

--- app/app.py
+++ app/app.py
@@ -1,8 +1,5 @@
-"""
-This module contains the main Flask application.
-"""
-
 import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,12 +7,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    """
-    Route for the root endpoint.
-
-    Returns:
-        str: A greeting message.
-    """
     return "Hello, World!"
 
 
